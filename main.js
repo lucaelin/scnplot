26fa4c00f836b307a11147db9083c65be0299098
Luca Haneklau <luca.ha@web.de>
2018-05-24 00:06:56 +0200

    Added X-Air support

--- main.js
+++ main.js
@@ -44,12 +44,12 @@ function createTable(classes, name, header, numch, datafn) {
   let h2 = document.createElement('h2');
   h2.innerText = name;
   dom.appendChild(h2);
-  
+
   let table = document.createElement('table');
   table.appendChild(createHeader(header));
   for (let ch = 1; ch<=numch; ch++) {
     table.appendChild(datafn(ch));
-  } 
+  }
   dom.appendChild(table);
   return dom;
 }
@@ -71,6 +71,9 @@ function getFXName(scn, short) {
 
 function getPhysicalIn(scn, id) {
   if(id==0) return '';
+  if(scn.version === 'X-Air') {
+    return id;
+  }
   return id.toString().padStart(2, '0') + ': ' + getPhysicalName(scn, id);
 }
 
@@ -79,7 +82,7 @@ function getPhysicalName(scn, id) {
   if(id <= 38) {
     return IN[Math.floor((id-1)/8)] + ' [' + ((id-1)%8+1) + ']' ;
   }
-  
+
   return ({
     39: 'USB-Player',
     40: 'USB-Player',
@@ -113,12 +116,13 @@ function getPhysicalName(scn, id) {
 function createChannel(scn, classes) {
   let name = 'Channel';
   let header = ['ch', 'link', 'name', 'lowcut', 'gate', 'dyn', 'insert', 'physical'];
-  let numch = 32;
+  let numch = Object.keys(scn.ch).length;
 
   return createTable(classes, name, header, numch, (id)=>{
     let ch = id.toString().padStart(2, '0');
     let config = scn.ch[ch];
     let link = scn.config.chlink.value[Math.floor((id-1)/2)];
+    let insert = scn.version!=='X-Air'?config.insert.value:[config.insert.value[0],'POST',config.insert.value[1]];
     return createTR(ch, [
       ch,                                                         // CHANNEL NUMBER
       [link==='ON', 'link'],                                      // STEREO LINK
@@ -126,54 +130,55 @@ function createChannel(scn, classes) {
       config.preamp.value[2]==='ON',                              // LOWCUT
       config.gate.value[0]==='ON',                                // GATE
       config.dyn.value[0]==='ON',                                 // COMPRESSOR
-      config.insert.value[0]==='ON'?config.insert.value[2]:'',    // INSERT
-      getPhysicalIn(scn, config.config.value[3]),                 // PHYSICAL  
+      insert[0]==='ON'?insert[2]:'',                              // INSERT
+      getPhysicalIn(scn, config.config.value[3]),                 // PHYSICAL
     ]);
   });
 }
 function createAux(scn, classes) {
   let name = 'Aux';
   let header = ['ch', 'link', 'name', 'physical'];
-  let numch = 8;
-  
+  let numch = Object.keys(scn.auxin).length;
+
   return createTable(classes, name, header, numch, (id)=>{
-    let ch = id.toString().padStart(2, '0');
+    let ch = scn.version!='X-Air'?id.toString().padStart(2, '0'):id.toString();
     let config = scn.auxin[ch];
     let link =  scn.config.auxlink.value[Math.floor((id-1)/2)];
     return createTR(ch, [
       ch,                                                         // CHANNEL NUMBER
       [link==='ON', 'link'],                                      // STEREO LINK
       [config.config.value[0], 'color-'+config.config.value[2]],  // NAME
-      getPhysicalIn(scn, config.config.value[3]),                 // PHYSICAL  
+      getPhysicalIn(scn, config.config.value[3]),                 // PHYSICAL
     ]);
   });
 }
 function createBus(scn, classes) {
   let name = 'Bus';
   let header = ['ch', 'link', 'name', 'dyn', 'insert'];
-  let numch = 16;
-  
+  let numch = Object.keys(scn.bus).length;
+
   return createTable(classes, name, header, numch, (id)=>{
-    let ch = id.toString().padStart(2, '0');
+    let ch = scn.version!='X-Air'?id.toString().padStart(2, '0'):id.toString();
     let config = scn.bus[ch];
     let link = scn.config.buslink.value[Math.floor((id-1)/2)];
+    let insert = scn.version!=='X-Air'?config.insert.value:[config.insert.value[0],'POST',config.insert.value[1]];
     return createTR(ch, [
       ch,                                                         // CHANNEL NUMBER
       [link==='ON', 'link'],                                      // STEREO LINK
       [config.config.value[0], 'color-'+config.config.value[2]],  // NAME
       config.dyn.value[0]==='ON',                                 // COMPRESSOR
-      config.insert.value[0]==='ON'?config.insert.value[2]:'',    // INSERT
+      insert[0]==='ON'?insert[2]:'',                              // INSERT
     ]);
   });
 }
 function createMatrix(scn, classes) {
   let name = 'Matrix';
   let header = ['ch', 'link', 'name', 'insert'];
-  let numch = 6;
-  
+  let numch = Object.keys(scn.mtx).length;
+
   return createTable(classes, name, header, numch, (id)=>{
-    let ch = id.toString().padStart(2, '0');
-    let config = scn.mtx[ch]; 
+    let ch = scn.version!='X-Air'?id.toString().padStart(2, '0'):id.toString();
+    let config = scn.mtx[ch];
     let link = scn.config.mtxlink.value[Math.floor((id-1)/2)];
     return createTR(ch, [
       ch,                                                         // CHANNEL NUMBER
@@ -186,11 +191,11 @@ function createMatrix(scn, classes) {
 function createMain(scn, classes) {
   let name = 'Main';
   let header = ['name', 'dyn', 'insert'];
-  let numch = 2;
+  let numch = scn.version!='X-Air'?2:1;
 
   return createTable(classes, name, header, numch, (id)=>{
-    let name = (['LR', 'M'])[id-1]; 
-    let config = scn.main[(['st', 'm'])[id-1]];
+    let name = (['LR', 'M'])[id-1];
+    let config = scn.version!='X-Air'?scn.main[(['st', 'm'])[id-1]]:scn.lr;
     return createTR(0, [
       name,                                                     // CHANNEL NUMBER
       config.dyn.value[0]==='ON',                               // COMPRESSOR
@@ -202,14 +207,14 @@ function createFx(scn, classes) {
   let name = 'FX-1';
   let header = ['ch', 'link', 'name', 'device', 'src'];
   let numch = 8;
-  
+
   return createTable(classes, name, header, numch, (id)=>{
     let ch = id.toString().padStart(2, '0');
     let name = 'FX'+Math.floor((id+1)/2)+(id%2?'L':'R');
     let rtnconfig = scn.fxrtn[ch];
     let fxconfig = scn.fx[Math.floor((id+1)/2)];
     let link = scn.config.fxlink.value[Math.floor((id-1)/2)];
-    
+
     return createTR(ch, [
       name,                                                             // CHANNEL NUMBER
       [link==='ON', 'link'],                                            // STEREO LINK
@@ -220,15 +225,15 @@ function createFx(scn, classes) {
   });
 }
 function createFxD(scn, classes) {
-  let name = 'FX-2';
+  let name = scn.version!='X-Air'?'FX-2':'FX';
   let header = ['ch', 'device', 'src'];
   let numch = 4;
-  
+
   return createTable(classes, name, header, numch, (id)=>{
-    id += 4;
+    id += scn.version!='X-Air'?4:0;
     let name = 'FX'+id;
     let fxconfig = scn.fx[id];
-    
+
     return createTR(id, [
       name,               // CHANNEL NUMBER
       fxconfig.value[0],  // TYPE
@@ -240,39 +245,62 @@ function createFxD(scn, classes) {
 function plot(scn) {
   let dom = document.querySelector('#out');
   dom.innerHTML = '';
-  let header = document.querySelector('h1');
-  header.innerText = scn.name;
-  
-  let one = document.createElement('div');
-  one.classList.add("row", "col", "s12", "m12", "l7");
+
+  if(scn.version!=='X-Air') {
+    let header = document.querySelector('h1');
+    header.innerText = scn.name;
+
+    let one = document.createElement('div');
+    one.classList.add("row", "col", "s12", "m12", "l7");
     one.appendChild(createChannel(scn, ['col', 's12', 'm12', 'l12']));
-  dom.appendChild(one);  
-  
-  let two = document.createElement('div');
-  two.classList.add("row", "col", "s12", "m12", "l5");
+    dom.appendChild(one);
+
+    let two = document.createElement('div');
+    two.classList.add("row", "col", "s12", "m12", "l5");
     two.appendChild(createBus(scn, ['col', 's12', 'm6', 'l12']));
     two.appendChild(createMatrix(scn, ['col', 's12', 'm6', 'l12']));
-  dom.appendChild(two);
-  
-  let three = document.createElement('div');
-  three.classList.add("row", "col", "s12", "m12", "l12");
+    dom.appendChild(two);
+
+    let three = document.createElement('div');
+    three.classList.add("row", "col", "s12", "m12", "l12");
     three.appendChild(createAux(scn, ['col', 's12', 'm6', 'l6']));
     three.appendChild(createMain(scn, ['col', 's12', 'm6', 'l6']));
-  dom.appendChild(three);
-  
-  let four = document.createElement('div');
-  four.classList.add("row", "col", "s12", "m12", "l12");
+    dom.appendChild(three);
+
+    let four = document.createElement('div');
+    four.classList.add("row", "col", "s12", "m12", "l12");
     four.appendChild(createFx(scn, ['col', 's12', 'm6', 'l6']));
     four.appendChild(createFxD(scn, ['col', 's12', 'm6', 'l6']));
-  dom.appendChild(four);
+    dom.appendChild(four);
+  } else {
+    let header = document.querySelector('h1');
+    header.innerText = 'X-Air Scene';
+
+    let one = document.createElement('div');
+    one.classList.add("row", "col", "s12", "m12", "l7");
+    one.appendChild(createChannel(scn, ['col', 's12', 'm12', 'l12']));
+    dom.appendChild(one);
+
+    let two = document.createElement('div');
+    two.classList.add("row", "col", "s12", "m12", "l5");
+    two.appendChild(createBus(scn, ['col', 's12', 'm6', 'l12']));
+    two.appendChild(createFxD(scn, ['col', 's12', 'm6', 'l6']));
+    two.appendChild(createMain(scn, ['col', 's12', 'm6', 'l6']));
+    dom.appendChild(two);
+  }
 }
 
 function convert2json(lines) {
-  let header = lines[0];
-  lines = lines.slice(1);
   let scn = {};
-  scn.version = header[0];
-  scn.name = header[1];
+  if(lines[0][0][0] !== '/') {
+    let header = lines[0];
+    lines = lines.slice(1);
+    scn.version = header[0];
+    scn.name = header[1];
+  } else {
+    scn.version = 'X-Air';
+    scn.name = 'Unnamed scene';
+  }
   lines.forEach((e)=>{
     if (!e[0] || e[0].split('/').length === 0) return;
     let path = e[0].split('/');
@@ -313,10 +341,10 @@ document.querySelector('input[type=file]').addEventListener('change', function(e
     }
 
     let scn = convert2json(out);
-    
+
     console.log(scn);
     document.querySelector('pre').innerText = JSON.stringify(scn, null, 4);
-    
+
     plot(scn);
   };
   reader.readAsText(files[0]);
